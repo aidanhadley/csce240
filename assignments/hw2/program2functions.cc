74cd99902a99775f21be82d53233130297b34edb
Aidan Hadley <asahadleya2@gmail.com>
2023-09-16 16:42:18 -0400

    Add SumDigits function

--- assignments/hw2/program2functions.cc
+++ assignments/hw2/program2functions.cc
@@ -1,2 +1,20 @@
 //  Copyright 2023 hadleya
-//  TODO: actually make the program lul
+#include<iostream>
+using std::cout;
+using std::endl;
+//  implement the protoypes found in the .h file
+//  expected cpplint error below - ignore!
+#include"program2functions.h"
+
+
+int SumDigits(int x) {
+    int output = 0;
+    //  basically used for absolute value
+    if( x < 0)
+        x = x * -1;
+
+    for (; x/10 > 0; x /= 10)
+        output += ( x % 10);
+    output += x;
+    return output;
+}
