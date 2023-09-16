902c02fd068072b3a55d4ca85d3fd4712144dd38
Aidan Hadley <asahadleya2@gmail.com>
2023-09-16 17:00:46 -0400

    Add logic for CountDigitMatch

--- assignments/hw2/program2functions.cc
+++ assignments/hw2/program2functions.cc
@@ -8,13 +8,36 @@ using std::endl;
 
 
 int SumDigits(int x) {
-    int output = 0;
+    int result = 0;
     //  basically used for absolute value
     if( x < 0)
         x = x * -1;
 
     for (; x/10 > 0; x /= 10)
-        output += ( x % 10);
-    output += x;
-    return output;
+        result += ( x % 10);
+    result += x;
+    return result;
+}
+
+bool IsPalindrome(int x) {
+    //  TODO: make logic for this one lol
+}
+
+int CountDigitMatch(int x, int check) {
+    int result = 0;
+    //  absolute value
+    if ( x < 0)
+        x = x * -1;
+
+    //  check and make sure check variable isn't more than 9 or negative
+    if (( check > 9) || (check < 0))
+        return -1;
+    for (; x/10 > 0; x /= 10) {
+        if ( (x % 10) == check )
+            result++;
+    }
+    if ( (x % 10) == check )
+        result++;
+    return result;
+
 }
